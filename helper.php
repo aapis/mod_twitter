0273d4a548d41ee196afc3da598a209ae323ff9e
Ryan Priebe <rpriebe@me.com>
2013-03-14 22:29:04 -0600

    Fixed up the options a bit, started adding different API calls

--- helper.php
+++ helper.php
@@ -9,11 +9,50 @@ defined('_JEXEC') or die;
 
 jimport('joomla.application.component.model');
 
-abstract class modTwitterHelper
-{
-	public static function getList(&$params)
-	{
+abstract class modTwitterHelper{
+	/**
+	 * [getList Get the data from Twitter]
+	 * @param  object $params The module parameters object
+	 * @return object
+	 */
+	public static function getList($params){
+		switch($params->get('type')){
+			case 'hashtag':
+				return modTwitterHelper::byHashTag($params);
+			break;
+
+			default: 
+				return modTwitterHelper::byUsername($params);
+		}
+	}
+
+	/**
+	 * [byUsername Pull data from Twitter by the username]
+	 * @param  object $params The module parameters object
+	 * @return object
+	 */
+	public static function byUsername($params){
 		$url = sprintf('https://api.twitter.com/1/statuses/user_timeline.json?screen_name=%s&count=%d', $params->get('screen_name'), $params->get('count'));
+		$rawdata = file_get_contents($url);
+		$data = array();
+
+		if(false === empty($rawdata) && false !== $rawdata){
+			$data = $rawdata;
+		}else {
+			$data[0] = new stdClass;
+			$data[0]->Message = sprintf('There are no recent tweets for the user <strong>%s</strong> or there was an error contacting the server.', $params->get('screen_name'));
+			$data[0]->Status = 'Error';
+		}
+
+		return (is_string($data) ? json_decode($data) : $data);
+	}
+	/**
+	 * [byHashTag Pull data from Twitter by a hash tag]
+	 * @param  object $params The module parameters object
+	 * @return object
+	 */
+	public static function byHashTag($params){
+		$url = sprintf('https://http://search.twitter.com/search.json?q=%s&rpp=%d', $hashtags, $params->get('count'));
 		$rawdata = file_get_contents($url);
 		$data = array();
 
--- tmpl/default.php
+++ tmpl/default.php
@@ -10,10 +10,24 @@ defined('_JEXEC') or die;
 $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 ?>
 
+<style type="text/css">
+	.mod_twitter {
+		margin-bottom: 5px;
+	}
+
+	.mod_twitter li {
+		list-style: none;
+	}
+
+	.mod_twitter ul {
+		padding: 0px;
+		margin-top: 0px;		
+	}
+</style>
+
 <div class="mod_twitter clearfix">
 	<?php if($module->showtitle): ?>
 		<div class="clearfix mod_latest_news_title">
-			<span class="icon icon-twitter ir">Twitter</span>
 			<h2 class="title"><?php echo $module->title; ?></h2>
 		</div>
 	<?php endif; ?>
@@ -27,14 +41,17 @@ $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 				<?php else : ?>
 				<?php
 					$item->text = preg_replace('/(http\:\/\/[a-zA-Z0-9\.\/]+[a-zA-Z0-9\/])/', '<a href="$1" target="_blank">$1</a>', $item->text);
-					$item->text = preg_replace('/\#([a-zA-Z0-9]+)/', '<a href="https://twitter.com/search?q=$1" target="_blank">#$1</a>', $item->text);
+					$item->text = preg_replace('/\#([a-zA-Z0-9]+)/', '<a href="https://twitter.com/search?q=%23$1" target="_blank">#$1</a>', $item->text);
 					$item->text = preg_replace('/\@([a-zA-Z0-9]+)/', '<a href="https://twitter.com/$1" target="_blank">@$1</a>', $item->text);
 				?>
 					<li>
-						<span class="user"><?php echo $item->user->name;?> <a href="http://twitter.com/<?php echo $item->user->screen_name; ?>" target="_blank">@<?php echo $item->user->screen_name; ?></a></span><br />
-						<span class="leadin"><?php echo $item->text; ?></span>
-						<br />
-						<span class="postdate"><em><?php echo date('F jS, Y', strtotime($item->created_at)); ?></em></span>
+						<?php if($params->get('show_user')): ?>
+							<p class="user"><?php echo $item->user->name;?> <a href="http://twitter.com/<?php echo $item->user->screen_name; ?>" target="_blank">@<?php echo $item->user->screen_name; ?></a></p>
+						<?php endif; ?>
+						<p class="leadin"><?php echo $item->text; ?></p>
+						<?php if($params->get('show_postdate', '1')): ?>
+							<span class="postdate"><em><?php echo date($params->get('postdate_format', 'F jS, Y'), strtotime($item->created_at)); ?></em></span>
+						<?php endif; ?>
 					</li>
 				<?php endif; ?>
 			<?php endforeach; ?>
@@ -42,5 +59,7 @@ $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 			<p>There are no recent tweets.</p>
 		<?php endif; ?>
 	</ul>
-	<p><a href="https://twitter.com/<?php echo $params->get('screen_name'); ?>" class="follow_us" target="_blank">Follow us on Twitter</p></a>
+	<?php if($params->get('show_follow_us_link')): ?>
+		<p><a href="https://twitter.com/<?php echo $params->get('screen_name'); ?>" class="follow_us" target="_blank"><?php echo $params->get('follow_us_link_text'); ?></p></a>
+	<?php endif; ?>
 </div>
